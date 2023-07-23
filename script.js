61eb84919016ab9c72dd09ad1b7ba295c3c283b9
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-22 23:20:08 -0400

    added function to evaluate and score user responses

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //Build a Quiz
+
+function quizAnswers(){
+	const quiz = document.getElementById('quizForm');
+	const correctAnswers = ['a','b','c'];
+	let userAnswers = [quizForm.q1.value,quizForm.q2.value,quizForm.q3.value];
+	let result = document.getElementById('results');
+	let score = 0;
+
+	userAnswers.forEach((answer,index) =>{
+		if(answer === correctAnswers[index]){
+			score ++;
+		}
+	});
+	let percentage = (score/correctAnswers.length)*100;
+	let grade = percentage.toFixed(2);
+	result.innerHTML = 'Your Score Is: ' + grade;
+}
